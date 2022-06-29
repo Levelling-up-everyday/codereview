e6ab1a61dd0c11f1746a2dae9a4b0d0399e9756f
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-06-29 21:15:43 +0530

    Create sortingvector.cpp

---
+++ sortingvector.cpp
@@ -0,0 +1,11 @@
+static bool compare(vector<int>& a, vector<int> & b)
+    {
+        if(a[0] == b[0])
+            return a[1] < b[1];
+        
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {   
+        int n = people.size();
+        
+        sort(people.begin(), people.end(), compare);
